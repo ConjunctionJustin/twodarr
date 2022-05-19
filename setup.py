64a8c3bb93a10aa5d8fe6eef45f811cf95999839
Justin Riegerix <1305jjr@gmail.com>
2022-05-19 08:49:08 -0500

    forgot to change version in setup.py once more

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='twodarr',
     packages=['twodarr'],
-    version='0.2',
+    version='0.3',
     license='MIT',
     author='Justin Riegerix',
     author_email='1305jjr@gmail.com',
     url='https://github.com/ConjunctionJustin/twodarr/tree/main',
-    download_url='https://github.com/ConjunctionJustin/twodarr/archive/refs/tags/v0.2.tar.gz',
+    download_url='https://github.com/ConjunctionJustin/twodarr/archive/refs/tags/v0.3.tar.gz',
     description=open('README.txt').read()
 )
