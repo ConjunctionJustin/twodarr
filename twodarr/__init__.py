f022f6c8285fdebe02b3827ad44eef4453b4a27b
Justin Riegerix <1305jjr@gmail.com>
2022-05-20 12:12:48 -0500

    added folder to import in __init__

--- twodarr/__init__.py
+++ twodarr/__init__.py
@@ -1,3 +1,3 @@
-from check_bound import check_bound
-from all_adj import all_adj
-from some_adj import some_adj
+from twodarr.check_bound import check_bound
+from twodarr.all_adj import all_adj
+from twodarr.some_adj import some_adj
